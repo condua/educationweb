82ebac7c9c7cdd8057c30cc315e1c4c1cf8b7ae8
condua <phuc.phanphuc2002@hcmut.edu.vn>
2026-01-31 00:01:04 +0700

    cập nhật giao diện VSAT

--- src/pages/vsat/data/examsList.js
+++ src/pages/vsat/data/examsList.js
@@ -11,14 +11,14 @@ export const EXAM_DATABASE = [
     tags: ["VSAT", "Đại học", "2025"],
     data: mathData, // Link tới file JSON
   },
-  {
-    id: "vsat-math-mock-02",
-    title: "ĐỀ LUYỆN TẬP SỐ 02",
-    subject: "Toán học",
-    duration: 60,
-    level: "Khó",
-    participants: 120,
-    tags: ["Luyện tập"],
-    data: mathData, // Demo
-  },
+  // {
+  //   id: "vsat-math-mock-02",
+  //   title: "ĐỀ LUYỆN TẬP SỐ 02",
+  //   subject: "Toán học",
+  //   duration: 60,
+  //   level: "Khó",
+  //   participants: 120,
+  //   tags: ["Luyện tập"],
+  //   data: mathData, // Demo
+  // },
 ];
